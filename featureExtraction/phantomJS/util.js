1bfab95897e8a30362b2d3037ef06b374fb22ffd
ajit jain <ajit.infotech@gmail.com>
2013-05-08 12:15:27 -0500

    rgb2hsv function

--- featureExtraction/phantomJS/util.js
+++ featureExtraction/phantomJS/util.js
@@ -46,3 +46,30 @@ var print = function(obj) {
 	str += "}";
 	console.log(str);
 };
+
+var rgb2hsv = function(r, g, b) {
+    var min = Math.min(r, g, b);
+    var max = Math.max(r, g, b);
+    var delta = max - min;
+    var h, s, v = max;
+
+    v = Math.floor(max / 255 * 100);
+    if ( max != 0 )
+        s = Math.floor(delta / max * 100);
+    else {
+        // black
+        return [0, 0, 0];
+    }
+
+    if( r == max )
+        h = ( g - b ) / delta;         // between yellow & magenta
+    else if( g == max )
+        h = 2 + ( b - r ) / delta;     // between cyan & yellow
+    else
+        h = 4 + ( r - g ) / delta;     // between magenta & cyan
+
+    h = Math.floor(h * 60);            // degrees
+    if( h < 0 ) h += 360;
+
+    return [h, s, v];
+};
